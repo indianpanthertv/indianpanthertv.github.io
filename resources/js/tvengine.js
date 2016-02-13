e466604e479366e5f71584592960a77a8de41b50
unknown <indianpantherbrand@gmail.com>
2016-02-13 19:47:11 +0530

    added ram database and refresh second functionality for tvplayer

--- resources/js/tvengine.js
+++ resources/js/tvengine.js
@@ -1,8 +1,9 @@
 /* AUTHOR : KAVULURU RADHA KRISHNA */
+/*
 
 var SchemaHour ={
 
-    /*  9 AM - 9 PM */
+    /*  9 AM - 9 PM *
 
     PL0910 : 'PLDcEtgLwQdoo9DxobB8QDEPyNvbPO1iOy',
     PL1011 :'PLDcEtgLwQdopmvr6Hvw7UEshZTyiXGlXN',
@@ -17,7 +18,7 @@ var SchemaHour ={
     PL1920 :'PLDcEtgLwQdooJ_SwuNfCcHgvslu0mbO0D',
     PL2021 :'PLDcEtgLwQdooC3tNFX1S3iX4n8mLP1_3A',
     
-    /*  9 PM - 9 AM */
+    /*  9 PM - 9 AM *
 
 
     PL2122 : 'PLDcEtgLwQdoo9DxobB8QDEPyNvbPO1iOy',
@@ -37,25 +38,294 @@ var SchemaHour ={
 
 
 
-
+*/
 var VideoData={
     
-    /*  */
+    /*  LAST INDEX LOG : 60*/
     
-    0 : {
-        
-        link:'7QU1nvuxaMA',title:'Audioslave - Like a Stone (Official Video)',duration:'00:04:59'
-        
+   /* --------------------------------- HINDI SONGS  *-------------------------------------------*/
+    
+    
+    0:{
+        title:'Lyrical: Aayat | Full Song with Lyrics | Bajirao Mastani' , link : 'vKDsAB1ccn0',duration:'00:04:19'
     },
-    1 : {
-        
-        link:'Mdo_A1Bj67w',title:'Tare Zameen par - Maa- (High Definition)',duration:'00:05:51'
+    
+    1:{
+        title:'Agar Tum Saath Ho FULL AUDIO Song | Tamasha | Ranbir Kapoor, Deepika Padukone | T-Series', link :'sK7riqg2mr4',duration: '00:05:42'
     },
-    2 : {
-        
-        link:'7oYqanTWruE',title:'Mera Jahan',duration:'00:06:02'
+    
+    2:{
+    title:'OFFICIAL: Mitti Di Khushboo FULL VIDEO Song | Ayushmann Khurrana | Rochak Kohli' , link : '8uJ-wOljP_s',duration: '00:04:46'
+    },
+    
+    3:{
+    title:'Samjhawan Unplugged | Humpty Sharma Ki Dulhania | Singer: Alia Bhatt', link : 'EoCz3Vx1pXg',duration: '00:03:46'
+    },
+    
+    4:{
+    	title:'Jeena Jeena HD Full Video Song | Badlapur 2015 | Atif Aslam,Varun Dhawan,Yami Gautam',link: 'SjsIws_8ozg',duration: '00:04:04'
+
+    },
+    
+    5:{
+    	title:'Suno Na Sangemarmar Arijit singh 1080p HD blu-ray',link :'PXh_8V7L1-Y',duration: '00:03:19'
+
+    },
+    
+    6:{
+    	title:'Pinga | Official Video Song | Bajirao Mastani | Deepika Padukone, Priyanka Chopra',link: ' xz-Vac75to4',duration: '00:03:32'
+
+    },
+    
+    7:{
+    	title:'Titli ( Full Song ) - Chennai Express - 2013 - Shahrukh Khan - Deepika Padukone - 1080p HD',link: 'Jet3KGOIh8w',duration: '00:05:51'
+
+    },
+    
+    8:{
+    	title:'"Tune Maari Entriyaan" | GUNDAY 2014 | Official Video Song | ft Ranveer, Arjun, Priyanka | HD 1080p',link: 'MubpNJAMu6c',duration: '00:05:26'
+
+    },
+    
+    9:{
+    	title:'Kabira Encore ™ ® Full video Song HD ᴴᴰ Yeh Jawaani Hai Deewani -',link: 'vTE2HVE6TAM',duration: '00:02:07'
+
+    },
+    
+    10:{
+    	title:'Main Rang Sharbaton Ka - Phata Poster Nikhla Hero I Shahid & Ileana | Atif Aslam & Chinmayi Sripaada',link: 'GnzZyGQi2ps',duration: '00:02:26'
+
+    },
+    
+    11:{
+    	title:'Kun Faya Kun Full Video Song Rockstar | Ranbir kapoor',link: 'T94PHkuydcw',duration: '00:06:21'
+
+    },
+    
+    12:{
+    	title:'Tare Zameen par - Maa- (High Definition)',link: 'Mdo_A1Bj67w',duration: '00:05:52'
+
+    },
+    
+    13:{
+    	title:'Swades - Yeh Jo Des Hai Tera HD 1920 x 816',link: 'iVN_VbATyCQ',duration: '00:05:26'
+
+    },
+    
+    14:{
+    	title:'Afghan Jalebi (Ya Baba) VIDEO Song | Phantom | Saif Ali Khan, Katrina Kaif | T-Series',link: '1s74PBDvbcA',duration: '00:02:46'
+
+    },
+    
+    15:{
+    	title:'Ambarsariya Fukrey Movie Full HD Video Song By VVAIDYA SAHAB',link: 'EMAP84puoQM',duration: '00:04:57'
+
+    },
+    
+    16:{
+    	title:'Pareshaan - Full Song - Ishaqzaade',link: 'zQl7zYkEP6M',duration: '00:05:41'
+
+    },
+    
+    17:{
+    	title:'Locha-E-Ulfat - 2 States | Official Song | Arjun Kapoor, Alia Bhatt',link: 'r_3fqt8UfXE',duration: '00:02:15'
+
+    },
+    
+    18:{
+        	title:'Dheere Dheere Se Meri Zindagi Video Song (OFFICIAL) Hrithik Roshan, Sonam Kapoor | Yo Yo Honey Singh',link: 'nCD2hj6zJEc',duration: '00:05:04'
+
+    
+    },
+    
+    19:{
+    	title:'MALHARI FULL SONG(BAJIRAO MASTANI)',link: 'omN1YM1CZUk',duration: '00:04:05'
+
+    },
+    
+    20:{
+    
+        	title:'MALHARI FULL SONG(BAJIRAO MASTANI)',link: 'omN1YM1CZUk',duration: '00:04:05'
+
+    },
+    
+   
+   /*-----------------------------------------------------------------------------------------------*/
+    
+     /* --------------------------------- ENGLISH SONGS  *-------------------------------------------*/
+    
+    
+    21:{
+    	title:'Selena Gomez - Slow Down (Official)',link: 'Z8eXaXoUJRQ',duration: '00:01:39'
+
+    },
+    22:{
+    	title:'Ariana Grande - Problem ft. Iggy Azalea',link: 'iS1g8G_njx8',duration: '00:03:27'
+
+    },
+    23:{
+    	title:'Fifth Harmony - Worth It (feat. Kid Ink) (Lyrics)',link: '_DNe_HXGp00',duration: '00:03:45'
+
+    },
+    24:{
+    	title:'Major Lazer & DJ Snake - Lean On (feat. MØ) (Official Music Video)',link: 'YqeW9_5kURI',duration: '00:02:58'
+
+    },
+    25:{
+    	title:'Maroon 5 - Animals',link: 'qpgTC9MDx1o',duration: '00:04:40'
+
+    },
+    26:{
+    	title:'Arash ft. Helena - Broken Angel (NEW OFFICIAL VIDEO HD 2010!!!).mp4',link: 'REY6ygmqWeI',duration: '00:06:45'
+
+    },
+    27:{
+    	title:'Justin Bieber - Sorry (Lyric Video)',link: '8ELbX5CMomE',duration: '00:03:18'
+
+    },
+    28:{
+    	title:'Katy Perry - Roar (Official)',link: 'CevxZvSJLk8',duration: '00:04:29'
+
+    },
+    29:{
+    	title:'Katy Perry - Wide Awake',link: 'k0BWlvnBmIE',duration: '00:04:36'
+
+    },
+    30:{
+    	title:'Taylor Swift - You Belong With Me',link: 'VuNIsY6JdUw',duration: '00:03:48'
+
+    },
+    31:{
+    	title:'Taylor Swift - Love Story',link: '8xg3vE8Ie_E',duration: '00:03:56'
+
+    },
+    32:{
+    	title:'Christina Perri - A Thousand Years [Official Music Video]',link: 'rtOvBOTyX00',duration: '00:04:47'
+
+    },
+    33:{
+    	title:'One Direction - What Makes You Beautiful',link: 'QJO3ROT-A4E',duration: '00:03:26'
+
+    },
+    34:{
+    	title:'One Direction - One Thing',link: 'Y1xs_xPb46M',duration: '00:03:17'
+
+    },
+    35:{
+    	title:'FROZEN - Let It Go Sing-along | Official Disney HD',link: 'L0MK7qz13bU',duration: '00:04:02'
+
+    },
+    36:{
+    	title:'Katy Perry - Firework',link: 'QGJuMBdaqIw',duration: '00:03:53'
+
+    },
+    37:{
+    	title:'Ke$ha - TiK ToK',link: 'iP6XpLQM2Cs',duration: '00:03:34'
+
+    },
+    38:{
+    	title:'Selena Gomez - The Heart Wants What It Wants (Official Video)',link: 'ij_0p_6qTss',duration: '00:04:35'
+
+    },
+    39:{
+    	title:'Selena Gomez & The Scene - Who Says',link: 'BzE1mX4Px0I',duration: '00:03:20'
+
+    },
+    40:{
+    	title:'Linkin Park - CASTLE OF GLASS (Official Video)',link: 'ScNNfyq3d_w',duration: '00:04:49'
+
+    },
+    41:{
+    	title:'Green Day - 21 Guns [Official Music Video]',link: 'r00ikilDxW4',duration: '00:05:26'
+
+    },
+    
+    
+    
+     /*-----------------------------------------------------------------------------------------------*/
+    
+     /* --------------------------------- TELUGU SONGS  *-------------------------------------------*/
+    ,
+    42:{
+    	title:'Yenno Yenno : Malli Malli Idi Rani Roju Full Video Songs',link: 'xwdjRkKqNYE',duration: '00:03:15'
+
+    },
+    43:{
+    	title:'Karthikeya Songs || Inthalo Ennenni Vinthalo || Nikhil Siddharth, Swati Reddy || Full HD 1080p..',link: 'TTQSUcKma3U',duration: '00:06:15'
+
+    },
+    44:{
+    	title:'Swamy Ra Ra | Neela Yevaru full song | Exclusive',link: 'ringSgIXWiU',duration: '00:02:55'
+
+    },
+    45:{
+    	title:'Mustafa Mustafa don't worry mustafa Prema Desam',link: 'b4xWUU6g8UA',duration: '00:06:02'
+
+    },
+    46:{
+    	title:'Endaro Full Video Song || Bhale Bhale Magadivoi || Nani, Lavanya Tripathi',link: 'mwNVq2EqEEE',duration: '00:04:02'
+
+    },
+    47:{
+    	title:'Yeto Vellipoyindhi Manasu - Yedhi Yedhi Video | Nani, Samantha',link: 'EjWC2QciLfI',duration: '00:05:48'
+
+    },
+    48:{
+    	title:'Konte Chuputho Video Song || Ananthapuram 1980 Movie Songs || Swati, Jai, Sasikumar',link: 'BCwsSk_KKrE',duration: '00:05:38'
+
+    },
+    49:{
+    	title:'Vennelave Vennelave - All Time Superhit Song - Merupu Kalalu Telugu Movie',link: '8zIm3ijR3O0',duration: '00:05:37'
+
+    },
+    50:{
+    	title:'Sakhi Songs | Sakhiya Cheliya Video Song | Madhavan, Shalini | Sri Balaji Video',link: 'i_Lsn8RoFv4',duration: '00:05:25'
+
+    },
+    51:{
+    	title:'Surya Son of Krishnan Movie | Monna Kanipinchavu Video Song | Surya, Sameera Reddy, Ramya',link: 'UyeHINaDqLI',duration: '00:07:57'
+
+    },
+    52:{
+    	title:'Kallu Moosi Yochisthey - Harris Jayaraj Karthik',link: 'Z8jv_BR8ls4',duration: '00:05:43'
+
+    },
+    53:{
+    	title:'tanemando GANESH.avi',link: 'QJ9On6va-Hw',duration: '00:05:39'
+
+    },
+    54:{
+    	title:'Darling Movie Songs | Neeve Neeve Song | Prabhas | Kajal Aggarwal | Prabhu | Shraddha Das',link: '0J8SLO4to0k',duration: '00:03:19'
+
+    },
+    55:{
+    	title:'Mr.Perfect Songs - Lite Teesko Song - Prabhas, Kajal, Taapsee',link: '9fiBXFzY9qc',duration: '00:04:41'
+
+    },
+    56:{
+    	title:'Regu Mullole - Chandamama Movie Songs - Navadeep Kajal Sivabalaji Sindhu menon',link: 'o9d1xnU117c',duration: '00:05:22'
+
+    },
+    57:{
+    	title:'Naalo Vusulaki - Chandamama Movie Songs - Navadeep Kajal Sivabalaji Sindhu menon',link: 'vHK2gV319iY',duration: '00:04:36'
+
+    },
+    58:{
+    	title:'Nuvvu Nenu Prema Songs | Preminche Premava Video Song | Suriya, Bhoomika | Sri Balaji Video',link: 'qS9OmYoX2cQ',duration: '00:06:01'
+
+    },
+    59:{
+    	title:'Surya Son of Krishnan Movie | Nalone Pongenu Narmada Video Song | Surya, Sameera Reddy, Ramya',link: '7dhKeHT2Bdk',duration: '00:06:56'
+
+    },
+    
+    60:{
+       	title:'Andala Rakshasi Full Songs HD - Yemito Ivale Video Song - Lavanya Tripati, Naveen, Rahul',link: 'rbCbho7aLYw',duration: '00:04:22'
+ 
+            
     }
     
+     /*-----------------------------------------------------------------------------------------------*/
    
 };
 
@@ -65,7 +335,7 @@ var VideoData={
 
 
 
-PlayList=[1,2,0];
+PlayList=[60,59,58,57,56,55,54,53,52,51,50,49];
 
 
 
@@ -140,7 +410,7 @@ function PLduration(PlayList){
 
 function PlaySimulate(PlayList,startTime){
 
-   // console.log(getSecondsDuration(startTime)+PLduration(PlayList));
+   //console.log(getSecondsDuration(startTime)+PLduration(PlayList));
     var endTimesec=getSecondsDuration(startTime)+PLduration(PlayList);
     
     var endTime=getDurationSeconds(endTimesec);
@@ -198,46 +468,59 @@ function getVideo(PlayList,startTime)
     
 }
 
-/* -------------------------------------------- PLAY PARAMETERS ----------------------------------------------*/
-var startTime="18:48:00";
-
-PlaySimulate(PlayList,startTime);
-var vidpar=getVideo(PlayList,startTime);
 
 
-
-
-
-/*-----------------------------------------------------------------------------------------------------------*/
-
-function refresh_playid()
-{
-    var date = new Date();
-    
-}
-
-
-
-var date = new Date();
-
-var hour = date.getHours();
-
- hour = (hour < 10 ? "0" : "") + hour;
-var next = parseInt(hour)+1;
- next = (next < 10 ? "0" : "") + next;
-
-var Schema = 'PL'+hour+next;
-
-
+function Set_Watch_Video(VideoData,vidpar){
 $(document).ready(function()
 {
     console.log(Schema);
     var play = VideoData[vidpar['video']]['link'];
     var startwhen=vidpar['start'];
-    var sdd=' <iframe width="100%" height="100%" src="https://www.youtube.com/embed/'+play+'?controls=0&showinfo=0&autohide=1&modestbranding=1&rel=0&t='+startwhen+'&version=3" frameborder="0" allowfullscreen></iframe>';
+    
+    //var time=getDurationSeconds(startwhen);
+    
+    
+    
+    
+    var sdd=' <iframe width="100%" height="100%" src="https://www.youtube.com/embed/'+play+'?autoplay=1&controls=0&showinfo=0&autohide=1&modestbranding=1&rel=0" frameborder="0" allowfullscreen></iframe>';
     $("#INDIANPANTHERTV").html(
     
     sdd
     
     );
 });
+}
+
+
+/* -------------------------------------------- PLAY PARAMETERS ----------------------------------------------*/
+var startTime="18:48:00";
+
+PlaySimulate(PlayList,startTime);
+var vidpar=getVideo(PlayList,startTime);
+Set_Watch_Video(VideoData,vidpar);
+
+
+
+
+/*-----------------------------------------------------------------------------------------------------------*/
+
+
+function refresh_playid(PlayList,startTime,vidpar)
+{
+    PlaySimulate(PlayList,startTime);
+ var vidparloc=getVideo(PlayList,startTime);
+    if(vidpar['video']==vidparloc['video']
+       {
+       
+       }
+       else{
+       Set_Watch_Video(VideoData,vidparloc);
+       vidpar=vidparloc;
+       }
+    
+}
+
+
+var tsecloop = setInterval(refresh_playid(),3000);
+
+
